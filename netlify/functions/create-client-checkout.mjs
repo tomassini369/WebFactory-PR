fd4cc34f42d63adf11dad365dcbdeadc6cad1b04
tomassini369 <tomassini.369@gmail.com>
2026-09-24 01:15:25 -0400

    refactor(v3): remove retired order utility dependency

--- netlify/functions/create-client-checkout.mjs
+++ netlify/functions/create-client-checkout.mjs
@@ -1,7 +1,7 @@
 import crypto from "node:crypto";
 import { createBookingHold } from "../lib/booking-engine.mjs";
 import { clientCommerceStore, commerceKey, getClientSite, getClientSiteBySlug } from "../lib/client-store.mjs";
-import { cleanText, publicBaseUrl, validEmail } from "../lib/order-store.mjs";
+import { cleanText, publicBaseUrl, validEmail } from "../lib/platform-utils.mjs";
 import { siteEntitlement } from "../lib/subscription-billing.mjs";
 import { calculateTax } from "../lib/webfactory-v3-domain.mjs";
 
